e91a6c459df2fb5315d30b8dcd67ecf94e48ee1b
Peach Wang <peachwang@outlook.com>
2021-09-04 08:28:51 +0800

    OperationLogCore::addOperationLog() 支持 无登录状态下调用 和 阻止回滚

--- src/Base/Model/Core/Log/OperationLogCore.php
+++ src/Base/Model/Core/Log/OperationLogCore.php
@@ -16,15 +16,17 @@ final class OperationLogCore extends BaseCore
     const COLLECTION_NAME = 'OperationLog';
     const ENTITY_PATH     = 'Log/OperationLog';
 
-    final public function addOperationLog($operation_type, $operation_context)
+    final public function addOperationLog($operation_type, $operation_context, $do_not_rollback = FALSE)
     {
         Kit::ensureString($operation_type);
         Kit::ensureArray($operation_context);
-        return $this->createEntity()
+        $operation_log = $this->createEntity()
             ->setOperationType($operation_type)
-            ->setOperator(Context::me())
             ->setOperationTime(Kit::now())
-            ->setOperationContext($operation_context)
-            ->addToCollection();
+            ->setOperationContext($operation_context);
+        $me = Context::me();
+        if (FALSE === is_null($me)) $operation_log->setOperator($me);
+        if (TRUE === $do_not_rollback) $operation_log->doNotRollback();
+        return $operation_log->addToCollection();
     }
 }
